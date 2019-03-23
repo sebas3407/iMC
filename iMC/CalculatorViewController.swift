f7ea099229e2de9db9775ee1555d60339f8613b2
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-03-23 17:11:39 +0100

    Added CalculatorViewController

---
+++ iMC/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorViewController.swift
+//  iMC
+//
+//  Created by Sebastian Ortiz Velez on 23/03/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
